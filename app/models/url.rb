43a0c270980df9cc91e4c84770134ad7aa186449
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-05 14:48:09 -0500

    add validation and remove bad urls

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,7 @@
 class Url < ActiveRecord::Base
-  
+  validates :long_url, presence: true
+  validate :has_http
+
   def self.random_phrase(number)
     string = ""
     number.times do
@@ -8,4 +10,34 @@ class Url < ActiveRecord::Base
     string
   end
 
+  def has_http
+    unless long_url =~ URI.regexp
+      errors.add(:long_url, "Not valid!")
+    end
+  end
+
 end
+
+__END__
+
+my_url = Url.first
+my_url.errors
+
+errors is a pseudo hash
+
+
+
+my_url.create
+
+_______
+
+my_url.new
+my_url.valid?
+my_url.save # assuming errors doesn't have stuff
+
+
+{
+  @base => <#Url:instance>
+  @messages => { long_url: "Not valid! Doesn't have http://", 
+                 click_count: "Can't be negative!" }
+}
